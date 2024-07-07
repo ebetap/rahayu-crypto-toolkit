a21f441f121376e341f88d7dcf618edd296ae2fb
Beta Priyoko (Muhammad) <beta.priyoko@students.amikom.ac.id>
2024-07-07 17:38:34 +0700

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,12 @@
+const CryptoToolkit = require('../src/index');
+const assert = require('assert');
 
+describe('CryptoToolkit', () => {
+  // Add your tests here
+  it('should create a new wallet', () => {
+    const toolkit = new CryptoToolkit({ apiKey: 'test-api-key' });
+    const wallet = toolkit.createWallet();
+    assert(wallet.publicKey);
+    assert(wallet.privateKey);
+  });
+});
